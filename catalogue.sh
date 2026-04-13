fea157d18e1a546251c28a93f5c8ee9b02dbb59f
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-13 12:14:16 +0530

    Roboshop Project

--- catalogue.sh
+++ catalogue.sh
@@ -2,10 +2,11 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
-useradd roboshop
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+useradd roboshop
 
 rm -rf /app
 mkdir /app
