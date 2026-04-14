bfb7943b1f7cd710a21a4070f80958fc09785f30
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-14 14:25:00 +0530

    Roboshop Project

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,24 @@
+app_name=catalogue
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $app_name.service /etc/systemd/system/$app_name.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 
 rm -rf /app
 mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/$app_name.zip
 
 cd /app
 npm install
 
 systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+systemctl enable $app_name
+systemctl start $app_name
 
 dnf install mongodb-mongosh -y
 
