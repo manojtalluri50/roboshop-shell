0031bd8fd76bf6839a265889bf67c37e43cb9890
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-14 15:34:43 +0530

    Roboshop Project

--- cart.sh
+++ cart.sh
@@ -1,10 +1,4 @@
 source ./common.sh
 app_name=cart
 
-cp cart.service /etc/systemd/system/cart.service
-
 NODEJS
-
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,10 @@
 source ./common.sh
 app_name=catalogue
 
-cp $app_name.service /etc/systemd/system/$app_name.service
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 NODEJS
-
-systemctl daemon-reload
-systemctl enable $app_name
-systemctl start $app_name
 
 dnf install mongodb-mongosh -y
 
