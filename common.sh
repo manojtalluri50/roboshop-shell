39a9e9e83660eaf5c796c675775f58038ff42ea7
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-14 16:49:20 +0530

    Roboshop Project

--- common.sh
+++ common.sh
@@ -2,19 +2,32 @@ dir_path=$(pwd)
 log_file=/tmp/roboshop.log
 
 SYSTEMD_SETUP(){
-  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service
 
-  systemctl daemon-reload
-  systemctl enable $app_name
-  systemctl start $app_name
+  echo Copy Application Service File
+  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service >$log_file
+
+  echo Start Application
+  systemctl daemon-reload >$log_file
+  systemctl enable $app_name >$log_file
+  systemctl start $app_name >$log_file
 }
 
 APP_PREREQ(){
+  echo Create Application User
   useradd roboshop >$log_file
+
+  echo Remove Application Directory
   rm -rf /app >$log_file
+
+  echo Create Application Directory
   mkdir /app >$log_file
+
+  echo Download Application Code
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip >$log_file
+
   cd /app >$log_file
+
+  echo Extract Application Code
   unzip /tmp/$app_name.zip >$log_file
 
 }
@@ -25,10 +38,13 @@ NODEJS(){
 
   echo Enable NodeJS 20 Version
   dnf module enable nodejs:20 -y >$log_file
+
+  echo Install NodeJS
   dnf install nodejs -y >$log_file
 
   APP_PREREQ
 
+  echo Install NodeJS Dependencies
   npm install >$log_file
 
   SYSTEMD_SETUP
@@ -55,3 +71,4 @@ PYTHON(){
   SYSTEMD_SETUP
 
 }
+
