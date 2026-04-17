e6a9588839cc7fa50f68da5b0ca45121b3d1583a
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-17 14:45:19 +0530

    Roboshop Project

--- frontend.sh
+++ frontend.sh
@@ -4,9 +4,6 @@ dnf module enable nginx:1.24 -y
 dnf install nginx -y
 cp nginx.conf /etc/nginx/nginx.conf
 
-systemctl enable nginx
-systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
 
 rm -rf /tmp/frontend.zip
@@ -16,4 +13,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+systemctl enable nginx
+systemctl start nginx
 systemctl restart nginx
